200c7894ba44584db58021ea13bc9bf4642738f9
Avneet Singh <146704346+programmeravneet@users.noreply.github.com>
2024-02-03 23:51:52 +0530

    Sum of prime numbers b/w 1 to 1000

--- prime.js
+++ prime.js
@@ -1,14 +1,69 @@
-for(let num=1;num<=10;num++){
-    let nod = 0
-    for(let div=1;div<=num;div++){
+/**
+ * prime number = 1, number
+ * 2 factor
+ * 2,3,5,7,,11,13,17,19,23,....
+ * 
+ * 
+ * range -1 to 100
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+for(let num =1;num<=100;num++){
+    var nod=0;
+
+
+    for(let div=2;div*div<num;div++){
         if(num%div==0){
-            nod=nod+1
+            nod=nod+1;
+            break;
+
+
         }
+   
+       
     }
-    if(nod==2){
-        console.log(num,"prime");
+    if(nod==0){
+        console.log(num,"  prime");
+    }else{
+        console.log(num," not prime")
     }
-    else{
-        console.log(num,"not prime");
-    }
+  
+ 
 }
+/**
+ * 1*36   36*1
+ * 2*18   18*2
+ * 3*12    12*3
+ * 4*9     9*4
+ * 6*6   6*6
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+/*
+1*100
+2*50
+4*25
+5*20
+10*10
+
+
+
+20*5
+25*4
+50*2
+100*1
+
+
+
+
+*/
--- prime2.js
+++ prime2.js
@@ -1,4 +1,5 @@
-for(let num=1;num<=10;num++){
+let sum=0
+for(let num=1;num<=1000;num++){
     let nod =0
     for(let div=2;div<num-1;div++){
         if(num%div==0){
@@ -7,9 +8,10 @@ for(let num=1;num<=10;num++){
         }
     }
     if(nod!=0){
-        console.log("not prime",num);
+        
     }
     else {
-        console.log("prime",num);
+        sum=sum+num
     }
 }
+console.log(sum);
